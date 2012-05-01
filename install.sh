2b556b9a0ed6cb89b0cbc98b8482742251970f10
Adam Jiang <chaoj@nvidia.com>
2012-05-01 11:34:40 +0900

    Added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+if [ -a ${HOME}/.emacs ]; then
+	mv ${HOME}/.emacs ${HOME}/_emacs
+	mv ${HOME}/.emacs.d ${HOME}/_emacs.d
+fi
+
+cp _emacs ${HOME}/.emacs
+cp -r _emacs.d ${HOME}/.emacs.d
+
