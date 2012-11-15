fdc4a32cfa84e999cdd11c7a5f740ddc71f811f2
Adam Jiang <jiang.adam@gmail.com>
2012-11-16 00:51:09 +0900

    ext: Add key-bindings for flymake

--- _emacs.d/site-lisp/pemacs-auxiliary.el
+++ _emacs.d/site-lisp/pemacs-auxiliary.el
@@ -3,4 +3,14 @@
 (unless (eq system-type 'windows-nt)
   (require 'xcscope))
 
+;; flymake
+(when (load-file
+       (concat pemacs-install-dir "extensions/flymake/flymake.el"))
+  (load-library "flymake-cursor"))
+;; enable flymake if possible
+;;(add-hook 'find-file-hook 'flymake-find-file-hook)
+;; keybindings
+(global-set-key [f10] 'flymake-goto-prev-error)
+(global-set-key [f11] 'flymake-goto-next-error)
+
 (provide 'pemacs-auxiliary)
