6ae5b45185c6f2952e557f57d5f96809e572c839
Adam Jiang <jiang.adam@gmail.com>
2013-09-11 09:48:32 +0900

    Remove active tab/softtab selection

--- _emacs.d/site-lisp/cc/pemacs-cc-edit.el
+++ _emacs.d/site-lisp/cc/pemacs-cc-edit.el
@@ -35,38 +35,5 @@
 		(arglist-cont-nonempty . +)
 		(template-args-cont . +))))
 
-;; Tabs or Spaces, that's a question
-; helper function
-(defun how-many-region (begin end regexp &optional interactive)
-  "Print number of non-trivial matches for ERGEXP in region.
-Non-interactive arguments are Begin for (regexp)"
-  (interactive "r\nsHow many matches for (regexp): \np")
-  (let ((count 0) opoint)
-    (save-excursion
-      (setq end (or end (point-max)))
-      (goto-char (or begin (point)))
-      (while (and (< (setq opoint (point)) end)
-		  (re-search-forward regexp end t))
-	(if (= opoint (point))
-	    (forward-char 1)
-	  (setq count (1+ count))))
-      (if interactive (message "%d occurrences" count))
-      count)))
-
-(defun infer-indentation-style ()
-  "If out source file uses tabs, we use tabs, if spaces spaces,
-and if neither, we use the current indent-tabs-mode."
-  (let ((space-count (how-many-region (point-min) (point-max) "^ "))
-	(tab-count (how-many-region (point-min) (point-max) "^\t")))
-    (if (> space-count tab-count) (setq indent-tabs-mode nil))
-    (if (> tab-count space-count) (setq indent-tabs-mode t))))
-
-(defun active-infer-indentation-style ()
-  "Active infer-indentation-style"
-  (setq indent-tabs-mode nil)
-  (infer-indentation-style))
-
-(add-hook 'c-mode-common-hook 'active-infer-indentation-style)
-
 (provide 'pemacs-cc-edit)
 ;;; edit.el ends here
